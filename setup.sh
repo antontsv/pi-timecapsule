8dd38543030e4b75297a052a16a8a492e78850c7
Anton Tsviatkou <a.tsviatkou@gmail.com>
2017-08-22 22:45:57 -0700

    Add a prompt to select a disk

--- setup.sh
+++ setup.sh
@@ -2,11 +2,18 @@
 
 disk_name="sdb"
 user="mac_timemachine"
+mount_point="/mnt/TimeMachine"
 sudo apt-get update
 sudo apt-get install -y netatalk
 lsblk -o KNAME,TYPE,SIZE,MODEL
-sudo mkdir /mnt/TimeMachine
-echo "/dev/$disk_name /mnt/TimeMachine auto defaults 0 2" | sudo tee -a /etc/fstab
+read -rp "Choose disk [example: sda]:" disk_name
+
+if ! lsblk "/dev/$disk_name" 1>/dev/null 2>&1;then
+    echo "Selected disk is not recognized: $disk_name"
+    exit 1
+fi;
+sudo mkdir "$mount_point"
+echo "/dev/$disk_name $mount_point auto defaults 0 2" | sudo tee -a /etc/fstab
 sudo mount "/dev/$disk_name"
 echo "/mnt/TimeMachine \"Time Machine\" options:tm" | sudo tee -a /etc/netatalk/AppleVolumes.defausudo useradd $user
 echo "Set password for $user..."
